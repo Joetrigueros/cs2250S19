49882077a28d24c3903f0bc0f4cbb25de2d7e27c
Joe Trigueros <Joetrigueros@mail.weber.edu>
2019-01-29 10:09:24 -0700

    Add constants for ICE LIQUID, and BOIL water states. Add a switch statement to print current water state.

--- mod4/temperature.c
+++ mod4/temperature.c
@@ -3,21 +3,28 @@
 //const int FAHRENHEIT = 1;
 #define CELSIUS 0
 #define FAHRENHEIT 1
-
+#define BOIL_CEL 100
+#define BOIL_FAHR 212
+#define FREEZE_CEL 0
+#define FREEZE_FAHR 32
+#define BOIL 1
+#define LIQUID 2
+#define ICE 3
 int main()
 {
-	
-//    float temp = 7.9;  //Celsius
+
+    //    float temp = 7.9;  //Celsius
     double in_temp, out_temp;
-	int choice;
+    int choice;
+    int water_state = 0;
     // 1) Ask user which temperature mode they want to use
     printf("Welcome to the temperature calculator\n");
     printf("Please enter your chice:\n");
     printf("\t0 for celsius or 1 for Fahrenheit: ");
     scanf("%d", &choice);
     // 2) Based o choice calculate celsius or Fahrenheit
-    
-    if(choice == 0)
+
+    if(choice == CELSIUS)
     {
         printf("Enter the temperature in Celsius: ");
         scanf("%lf", &in_temp);
@@ -35,20 +42,67 @@ int main()
     }
     else
     {
-        
+
         printf("Sorry, you did not enter 0 or 1\nAdios amigo\n");
         return 0;
     }
+    // Task 2: 
 
-/*
-    printf("The temperature is t int Cf Celsius\n", temp);
-	printf("what is the temperature in Ogden now in Celsius: \n");
-	// use %f to capture float information
-	// use %lf to capture double information
-	scanf("%f", &temp);
-	// convert F to C
-	f_temp =(temp * 9/5)+ 32;
-	printf("I see, your temp is %lf in Fahrenheit.\n",f_temp);
-*/
+    //Test for boiling point, freezing point, and liquid point
+    if(choice == CELSIUS)
+    {
+        if(in_temp >= BOIL_CEL)
+        {
+            water_state = BOIL;
+        }
+        // &&->AND
+        else if (in_temp > FREEZE_CEL && in_temp < BOIL_CEL)
+        {
+            water_state = LIQUID;
+        }
+        else
+        {
+            water_state = ICE;
+        }
+    }
+    else //Fahrenheit
+    {
+        if(in_temp >= BOIL_FAHR)
+        {
+            water_state = BOIL;
+        }
+        // &&->AND
+        else if (in_temp > FREEZE_FAHR && in_temp < BOIL_FAHR)
+        {
+            water_state = LIQUID;
+        }
+        else
+        {
+            water_state = ICE;
+        }
+    }
+    // Switch
+    switch(water_state)
+    {
+        case BOIL:
+            printf("Your water is boiling\n");
+            break; //exit your switch
+        case LIQUID:
+            printf("Your water is liquid\n");
+            break;
+        case ICE:   
+            printf("Your water is ice\n");
+            break;
+    }
+    /*
+       printf("The temperature is t int Cf Celsius\n", temp);
+       printf("what is the temperature in Ogden now in Celsius: \n");
+    // use %f to capture float information
+    // use %lf to capture double information
+    scanf("%f", &temp);
+    // convert F to C
+    f_temp =(temp * 9/5)+ 32;
+    printf("I see, your temp is %lf in Fahrenheit.\n",f_temp);
+    */
     return 0;
 }
