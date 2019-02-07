6d24d2a69d501ed9d75a0131b032364e08d1771b
Joe Trigueros <Joetrigueros@mail.weber.edu>
2019-02-07 10:19:11 -0700

    First commit

---
+++ mod5/arrays.c
@@ -0,0 +1,46 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  arrays.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/07/2019 10:06:40 AM
+ *       Revision:  none
+ *       Compiler:  gcc arrays.c -o arrays.out
+ *
+ *         Author:  Joe Trigueros (), Joetrigueros@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+#define NUM 3
+// Function Prototypes
+
+// Main Function
+int main()
+{
+//    int i1, i2, i3;
+    //Arrays use index notation to access members
+    int grades[NUM]; // array of size NUM
+
+    for(int i = 0; i < NUM; i++)
+    {
+        printf(" Enter a number: ");
+        // Use the address (&) operator
+        // Make sure you specify the index position in [ ]
+        scanf("%d", &grades[i]); //TODO: how do I save the values???
+    }
+    for(int i = 0; i < NUM; i++)
+    {
+        printf("%d number is [%d]\n",i+1,  grades[i]);
+    }
+    return 0;
+}
+// Function Definitions
+
+
