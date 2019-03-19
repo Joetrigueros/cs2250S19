b1db96907edf36a79b476588bd30e73922044e3c
Joe Trigueros <Joetrigueros@mail.weber.edu>
2019-03-19 09:02:47 -0600

    Fixed dereference to double pointer

---
+++ mod10/struct_test.c
@@ -0,0 +1,46 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  struct_test.c
+ *
+ *    Description:  Define a structure with pointers
+ *
+ *        Version:  1.0
+ *        Created:  03/19/2019 08:52:49 AM
+ *       Revision:  none
+ *       Compiler:  gcc struct_test.c -o struct_test.out -lm
+ *          Usage:  ./struct_test.out
+ *
+ *         Author:  Joe Trigueros (), Joetrigueros@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+// Constants & Globals
+typedef struct Test
+{
+    int idNum;          // store an integer
+    double* ptPay;      // store and address of a double
+
+}Test;
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+    Test emp;
+    double pay = 456.23;
+
+    emp.idNum = 12345;
+    emp.ptPay = &pay;
+
+    printf("Employee number %d was paid $[%6.2lf]\n", emp.idNum, *emp.ptPay);
+
+    return 0;
+}
+// Function Definitions
+
+
