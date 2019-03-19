0b75e850319008ed5872efb2ea045c0ebba3366e
Joe Trigueros <Joetrigueros@mail.weber.edu>
2019-03-19 09:34:09 -0600

    First Commit

---
+++ mod10/tele_type.c
@@ -0,0 +1,51 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  tele_type.c
+ *
+ *    Description:  First look into LinkedList
+ *
+ *        Version:  1.0
+ *        Created:  03/19/2019 09:03:17 AM
+ *       Revision:  none
+ *       Compiler:  gcc tele_type.c -o tele_type.out -lm
+ *          Usage:  ./tele_type.out
+ *
+ *         Author:  Joe Trigueros (), Joetrigueros@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+// Constants and Globals
+#define MAXNAME 30
+#define MAXPHONE 15
+struct TeleType
+{
+    char name[MAXNAME];
+    char phoneNum[MAXPHONE];
+    struct TeleType* nextaddr;     // "should" be the last member
+};
+typedef struct TeleType Tele;     // "should" be the last member
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+    Tele t1 = {"Acme, Junio", "(801) 891-2344"};
+    Tele t2 = {"Martinez, Tino", "(801) 567-2344"};
+    Tele t3 = {"Weber, Waldo", "(801) 638-0011"};
+    Tele* first;   // pointer(address) to structure
+
+    first = &t1;  // get the address of a Tele structure
+    t1.nextaddr = &t2;   //set the address of the next structure
+    t2.nextaddr = &t3;   //set the address fo the next structure
+    t3.nextaddr = NULL;  //set the address of NULL, last member
+    printf("%s\n %s\n %s\n", first->name, t1.nextaddr->name, t2.nextaddr->name);
+
+    return 0;
+}
+// Function Definitions
+
+
