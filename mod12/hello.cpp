189d6b3b12532799d036b8775c9ff53cc28bb473
Joe Trigueros <Joetrigueros@mail.weber.edu>
2019-04-02 09:04:43 -0600

    First C++ program

---
+++ mod12/hello.cpp
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.cpp
+ *
+ *    Description:  First hellp world in C++
+ *
+ *        Version:  1.0
+ *        Created:  04/02/2019 08:49:46 AM
+ *       Revision:  none
+ *       Compiler (C):    gcc hello.cpp -o hello.cpp.out
+ *       Compiler (C++):  g++ hello.cpp -o hello.cpp.out
+ *
+ *         Author:  Joe Trigueros (), Joetrigueros@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+//For C++ Code
+#include <iostream>
+using namespace std;
+
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main(int argc, char* argv[])
+{
+    int num;
+    // Print out
+    cout << "Hello C++" << endl;
+    cout << "Enter a number: " << endl;
+    // for input use: cin
+    cin >> num;
+    cout << "You entered: " << num << " number."  << endl;
+
+    return 0;
+}
+// Function Definitions
+
+
